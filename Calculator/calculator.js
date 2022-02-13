ff5878f689e020919ea533a4b173ba57a4db53cf
RohanTapulli1311 <74230154+RohanTapulli1311@users.noreply.github.com>
2022-02-13 22:18:19 +0530

    done server setup

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,3 +1,11 @@
 const express = require("express")
 
 const app = express()
+
+app.get("/", function(req,res){
+    res.send("<h1>Hello world</h1>")
+})
+
+app.listen(4001,function(){
+    console.log("server is listening to port 4001")
+})
