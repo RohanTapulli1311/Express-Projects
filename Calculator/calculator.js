dfeba37c3019d413ad8e935e3e9e30cd43eac9b2
RohanTapulli1311 <74230154+RohanTapulli1311@users.noreply.github.com>
2022-02-15 19:52:40 +0530

    completed basic calculator web dev

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,9 +1,21 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 
 const app = express()
 
+app.use(bodyParser.urlencoded({extended:true}))
+
 app.get("/", function(req,res){
-    res.send("<h1>Hello world</h1>")
+    // res.send("<h1>Hello world</h1>")
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function(req,res){
+    console.log(req.body)
+    const num1 = Number(req.body.num1)
+    const num2 = Number(req.body.num2)
+    const sum = num1+num2
+    res.send("The sum of two numbers is = "+sum)
 })
 
 app.listen(4001,function(){
