0857439c2c046b3ecb054e443c7f60ca123ef06b
RohanTapulli1311 <74230154+RohanTapulli1311@users.noreply.github.com>
2022-02-16 22:39:58 +0530

    modified server

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-
+const family = {
+  "Rohan":"me",
+  "Sapna" : "mom",
+  "Shaji" : "Dad",
+  "Nidhi" : "Sister"
+}
 const app = express()
-
+app.use(express.static(__dirname+'/images'));
 app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/", function(req,res){
@@ -31,11 +36,30 @@ app.post("/", function(req,res){
 })
 
 app.get("/family", function(req,res){
-  console.log(req.body)
-  
+  res.sendFile(__dirname + "/family.html")
+
 })
+
+app.post("/family", function (req,res) {
+   console.log(req.body)
+   let fName = req.body.name
+   let relation = ""
+   if (fName in family){
+     console.log(fName+"1")
+    relation = family[fName]
+   }
+   if(relation === ""){
+    console.log(fName)
+     res.send("<h1>No relation</h1>")
+   }
+   else{
+    res.send("The relation is = "+relation)
+   }
+
+  })
 
 
 app.listen(4001,function(){
     console.log("server is listening to port 4001")
 })
+
