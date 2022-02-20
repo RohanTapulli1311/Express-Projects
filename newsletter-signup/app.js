56ff174f3f38ae17f2f1e6a3a7a82693312b12ab
RohanTapulli1311 <74230154+RohanTapulli1311@users.noreply.github.com>
2022-02-20 22:02:14 +0530

    included public folder and added implemented server

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -2,9 +2,17 @@ const express = require("express")
 const bodyParser = require("body-parser")
 // const request = require("request")
 const app = express()
-
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"))
 app.get("/", function(req,res){
     res.sendFile(__dirname+"/signup.html")
+})
+
+app.post("/", function(req,res){
+    const fname = req.body.fname
+    const lname = req.body.lname
+    const email = req.body.email
+    console.log(fname+" "+lname+" "+email)
 })
 
 app.listen(3002, function(){
