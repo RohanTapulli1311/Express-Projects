f63b861067392644aa6457fd3adfc2f3ac2b1eeb
RohanTapulli1311 <74230154+RohanTapulli1311@users.noreply.github.com>
2022-02-20 21:28:13 +0530

    added new project'

---
+++ newsletter-signup/app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+// const request = require("request")
+const app = express()
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/signup.html")
+})
+
+app.listen(3002, function(){
+    console.log("server listening on port 3002")
+})
