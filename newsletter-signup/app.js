be8752d2e9b3a1a128e8207bbdcde1a3a18881f1
RohanTapulli1311 <74230154+RohanTapulli1311@users.noreply.github.com>
2022-02-22 22:26:34 +0530

    solved privacy issue, and added failure and success pages

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -1,8 +1,12 @@
+
+const myModule = require('./config');
 const express = require("express")
 const bodyParser = require("body-parser")
 const https = require("https")
 const { urlToHttpOptions } = require("url")
 // const request = require("request")
+const apikey = myModule.config.api_key_mchp
+const aud_id = myModule.config.audience_id
 const app = express()
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
@@ -30,28 +34,44 @@ app.post("/", function(req,res){
     }
     const jsonData = JSON.stringify(data)
 
-    const url = "https://us14.api.mailchimp.com/3.0/lists/c5f09e78e4"
+    const url = `https://us14.api.mailchimp.com/3.0/lists/${aud_id}`
 
     const options = {
         method : "POST",
-        auth: "rohan1:300f81fd5936233401ec73b925f2cfdc-us14"
+        auth: `rohan1:${apikey}`
     }
 
     const request = https.request(url, options, function(response){
+        console.log(response.statusCode)
+        // if(response.statusCode != 200){
+        //     res.sendFile(__dirname+"/failure.html")
+        // }
+        // else{
+        //     res.sendFile(__dirname+"/success.html")
+        // }
+        
         response.on("data", function(data){
+            const jsonfile = JSON.parse(data)
             console.log(JSON.parse(data))
+            console.log(jsonfile.error_count)
+            if(jsonfile.error_count === 1){
+                res.sendFile(__dirname+"/failure.html")
+            }
+            else{
+                res.sendFile(__dirname+"/success.html")
+            }
         })
     })
+   
 
     request.write(jsonData)
     request.end()
 })
 
+app.get("/failure", function (req,res) {
+    res.sendFile(__dirname+"/signup.html")
+  })
 app.listen(3002, function(){
     console.log("server listening on port 3002")
 })
 
-  //300f81fd5936233401ec73b925f2cfdc-us14
-
-  //Audience id
-  //c5f09e78e4
