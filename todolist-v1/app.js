99d1f9e71fad497c5694790d5ca2df0f7ce1d585
RohanTapulli1311 <74230154+RohanTapulli1311@users.noreply.github.com>
2022-02-27 21:39:23 +0530

    added form to input new tasks

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -4,22 +4,39 @@ const bodyParser = require("body-parser")
 const app = express()
 
 app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended:true}))
 app.get("/", function(req,res){
    const today = new Date()
-   dayList = ["monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-   dayType = ""
-   day = ""
-   if(today.getDay() === 6|| today.getDay() === 7){
-       dayType = "weekend"
-       day = dayList[today.getDay()]
-   }
-   else{
-        dayType = "weekday"
-       day = dayList[today.getDay()]
+//    dayList = ["monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+//    dayType = ""
+//    day = ""
+
+const options = {
+    weekday : "long",
+    day: "numeric",
+    month : "long"
+}
+
+ const day  = today.toLocaleDateString("en-US",options)
+
+//    if(today.getDay() === 6|| today.getDay() === 7){
+//        dayType = "weekend"
+//        day = dayList[today.getDay()]
+//    }
+//    else{
+//         dayType = "weekday"
+//        day = dayList[today.getDay()]
   
-   }
-   res.render("lists",{kindOfDay: day, type: dayType })
+//    }
+
+
+    res.render("lists",{kindOfDay: day})
  
+})
+
+app.post("/", function(req,res){
+ const newItem = req.body.newItem
+ console.log(newItem)
 })
 
 app.listen("3000", function(){
