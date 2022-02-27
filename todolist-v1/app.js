ce1e204a098ea1f9d6b55c78ee63c66116802650
RohanTapulli1311 <74230154+RohanTapulli1311@users.noreply.github.com>
2022-02-27 22:01:36 +0530

    implemented add to list feature

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -5,6 +5,9 @@ const app = express()
 
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended:true}))
+
+const items = ["Buy Food","Cook Food","Eat Food"]
+
 app.get("/", function(req,res){
    const today = new Date()
 //    dayList = ["monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
@@ -30,13 +33,16 @@ const options = {
 //    }
 
 
-    res.render("lists",{kindOfDay: day})
+    res.render("lists",{kindOfDay: day, newListItem: items})
  
 })
 
 app.post("/", function(req,res){
  const newItem = req.body.newItem
  console.log(newItem)
+ items.push(newItem)
+
+ res.redirect("/")
 })
 
 app.listen("3000", function(){
