c253100a80895a3a8f542980dda6ae282c314a72
RohanTapulli1311 <74230154+RohanTapulli1311@users.noreply.github.com>
2022-03-08 23:57:11 +0530

    modified db connection

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/todolistDB")
+mongoose.connect("mongodb+srv://admin-Rohan:Test1234@cluster0.shkqb.mongodb.net/todolistDB")
 
 const itemSchema = new mongoose.Schema({
   name: String
