c228a133a6f8a38e64d21683350ea0eac5a98232
RohanTapulli1311 <74230154+RohanTapulli1311@users.noreply.github.com>
2022-04-05 23:21:40 +0530

    GET, POST, DELETE done for articles

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -17,6 +17,79 @@ const articleSchema = {
 
 const Article = mongoose.model("Article",articleSchema)
 
+app.get("/articles",function(req,res){
+    Article.find(function(err,foundArticles){
+        if(err){
+           // console.log(err)
+           res.send(err)
+        }
+        else{
+            //console.log(foundArticles)
+            res.send(foundArticles)
+        }
+    })
+})
+
+//POST
+app.post("/articles",function (req,res) {
+    console.log(req.body.title)
+    console.log(req.body.content)
+
+    const newArticle = new Article({
+        title : req.body.title,
+        content : req.body.content
+    })
+     newArticle.save(function(err){
+        if(err){
+            res.send(err)
+        }
+        else{
+            res.send("successfully added a new article")
+        }
+     })
+  })
+
+  app.delete("/articles",function (req,res) {
+      Article.deleteMany(function (err) {
+          if(err){
+              res.send(err)
+
+          }
+          else{
+              res.send("Successfully deleted all entries")
+          }
+        })
+    })
+
 app.listen(3000, function () { 
     console.log("server started on port 3000")
  })
+ 
+ //test data
+//  {
+//     "_id": "6246fb28a875950403c85ffa",
+//     "title": "REST",
+//     "content": "REST stands for REpresentational State Transfer. It is an architectural style for designing API's"
+// },
+// {
+//     "_id": "6246fbc1a875950403c85ffb",
+//     "title": "Bootstrap",
+//     "content": "This is a framework developed by Twitter that contains pre-made front-end templates for web design"
+// },
+// {
+//     "_id": "6246fbd3a875950403c85ffc",
+//     "title": "DOM",
+//     "content": "The Document Object Model is like an API for interacting with our HTML"
+// },
+// {
+//     "_id": "624c7ddca25909f012e3f3d4",
+//     "title": "Zlatan Ibrahimovic",
+//     "content": "Lions dont compare them to humans",
+//     "__v": 0
+// },
+// {
+//     "_id": "624c7f64be423d0941a9047a",
+//     "title": "Jack Bauer",
+//     "content": "Jack went into a slide, slide slid down",
+//     "__v": 0
+// }
