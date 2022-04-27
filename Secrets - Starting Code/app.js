d9add9e95254d8ad1b0d3c0e6526cbc5618395b8
RohanTapulli1311 <74230154+RohanTapulli1311@users.noreply.github.com>
2022-04-27 23:47:37 +0530

    modified app.js

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -10,6 +10,7 @@ const bcrypt = require("bcrypt")
 const session = require("express-session")
 const passport = require("passport")
 const passportLocalMongoose= require("passport-local-mongoose")
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express()
 
 const saltRounds = 10
@@ -46,6 +47,18 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/",function (req,res) {
     res.render("home")
