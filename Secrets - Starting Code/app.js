e2ef9733f11e2737f4502f5c740f39d1b5d1d55f
RohanTapulli1311 <74230154+RohanTapulli1311@users.noreply.github.com>
2022-04-28 17:58:23 +0530

    completed facebook auth

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -11,6 +11,7 @@ const session = require("express-session")
 const passport = require("passport")
 const passportLocalMongoose= require("passport-local-mongoose")
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const findOrCreate = require("mongoose-findorcreate")
 const app = express()
 
@@ -36,6 +37,7 @@ const userSchema = new mongoose.Schema({
     email : String,
     password: String,
     googleId:String,
+    facebookId:String,
     secret:String
 });
 
@@ -70,6 +72,17 @@ passport.use(new GoogleStrategy({
     });
   }
 ));
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: "http://localhost:3000/auth/facebook/secrets"
+},
+function(accessToken, refreshToken, profile, cb) {
+  User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
 
 app.get("/",function (req,res) {
     res.render("home")
@@ -81,6 +94,15 @@ app.get("/",function (req,res) {
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
     // Successful authentication, redirect secrets.
+    res.redirect('/secrets');
+  });
+  app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+ app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
     res.redirect('/secrets');
   });
 
